76967694c1e1167314e31699d95df5c9e5e95a1b
weibinw219 <weibinw0219@gmail.com>
2021-04-14 12:37:49 -0400

    add express set up and server listener

--- index.js
+++ index.js
@@ -7,10 +7,27 @@ const mongoURL = "mongodb+srv://dbUser:0000@cluster0.x7xyu.mongodb.net/LBAL?retr
 // configuration options to use when connecting to the database
 const connectionOptions = {useNewUrlParser: true, useUnifiedTopology: true}
 
+
+// express setup
+const express = require("express");
+const app = express();
+app.use(express.json())
+const HTTP_PORT = process.env.PORT || 8080;
+
+
+
+// connect to database & start server
+// ----------------------------------
+const onHttpStart = () => {
+    console.log(`Server has started and is listening on port ${HTTP_PORT}`)
+}
+
+
 // connect to the database and check that it worked
 mongoose.connect(mongoURL, connectionOptions).then(
     () => {
         console.log("DB connected successfully!")
+        app.listen(HTTP_PORT, onHttpStart); 
     }
 ).catch(
     (err) => {
@@ -18,3 +35,4 @@ mongoose.connect(mongoURL, connectionOptions).then(
         console.log(err)
     }
 )
+
